a844404879b3057e32b209cc8347920692e92928
brandonbrown245 <45397055+brandonbrown245@users.noreply.github.com>
2019-01-07 17:07:37 +0000

    Update index.js

--- index.js
+++ index.js
@@ -55,26 +55,6 @@ bot.on("message", async message => {
 
   let commandfile = bot.commands.get(cmd.slice(prefix.length));
   if(commandfile) commandfile.run(bot,message,args);
-
-  
-   bot.on("message", async message => {
-        if (message.author.bot) return
-        let modlogid = 530862540239011852 //Channel ID for Mod Log
-        let modlog = message.guild.channels.get(modlogid)
-        const profanities = require('profanities');
-        let profEmbed = new Discord.RichEmbed()
-            .setDescription(`User ${message.author}, has been caught swearing!\nUserID: ${message.author.id} - UserTag: ${message.author.tag}`)
-        for (x = 0; x < profanities.length; x++) {
-            if (message.content.toUpperCase() == profanities[x].toUpperCase()) {
-                // if (message.content == profanities)
-                message.author.send(`Hinami Security, Swearing is not allowed here. Please refrain from swearing, ${message.author}`)
-                modlog.send(profEmbed).catch(() => message.guild.owner(`Mod-log hasn't been configured, any discord profanity triggers will be sent directly to you.\n\`${message.content}\`\nBy User: ${message.author}\nBy UID: ${message.author.id}\nBy UserTag: ${message.author.tag}`))
-                modlog.send(`\`Message Sent\`: ${message.content}`)
-                message.delete();
-                return;
-            }
-        }
-    })
   
 });
 bot.login(process.env.token);
