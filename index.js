b76bcadd8430b33f81730de2ccef1935aa608c0d
brandonbrown245 <45397055+brandonbrown245@users.noreply.github.com>
2019-01-27 15:08:26 +0000

    Update index.js

--- index.js
+++ index.js
@@ -7,24 +7,6 @@ bot.commands = new Discord.Collection();
 let purple = botconfig.purple;
 
 fs.readdir("./commands/", (err, files) => {
-
-  
-  client.on('guildMemberAdd', member => {
-    let addRole_Unregistered = member.guild.roles.find(role => role.name == "Unregistered");
-    console.log("\x1b[35m\x1b[1m[join]\x1b[0m " + member.user.tag);
-    member.send("Hello! We are glad you joined our Cult! \nPlease read the rules in the #rules channel. You'll also find instructions on how to gain access to the server there.\n\nThanks and have fun! :)").then(m => {
-        let URLembed = new Discord.RichEmbed()
-            .setTitle("(Link to the #rules channel)")
-        member.send(URLembed);
-    });
-    member.addRole(addRole_Unregistered);
-});
-
-client.on("guildMemberRemove", member => {
-    console.log("\x1b[31m\x1b[1m[leave]\x1b[0m " + member.user.tag);
-    var botLogs = client.channels.find(channel => channel.id == "536570190054293524");
-    return botLogs.send(`❌ \`${member.user.tag}\` has left the server`);
-});
   
   if(err) console.log(err);
   let jsfile = files.filter(f => f.split(".").pop() === "js");
