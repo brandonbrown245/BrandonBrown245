6b9edafcf789cdce90747854e8d10f5705a9f7f2
brandonbrown245 <45397055+brandonbrown245@users.noreply.github.com>
2019-04-13 18:59:08 +0100

    Create warn.js

---
+++ commands/warn.js
@@ -0,0 +1,62 @@
+
+const Discord = require("discord.js");
+const fs = require("fs");
+const ms = require("ms");
+let warns = JSON.parse(fs.readFileSync("./warnings.json", "utf8"));
+
+module.exports.run = async (bot, message, args) => {
+
+  //!warn @daeshan <reason>
+  if(!message.member.hasPermission("BAN_MEMBERS")) return message.reply("Sorry sir you are not an Administrator so you can't use this command.");
+  let wUser = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0])
+  if(!wUser) return message.reply("Sorry sir i can't find the person you are looking for.");
+  if(wUser.hasPermission("BAN_MEMBERS")) return message.reply("Sorry sir you can't do that.");
+  let reason = args.join(" ").slice(22);
+
+  if(!warns[wUser.id]) warns[wUser.id] = {
+    warns: 0
+  };
+
+  warns[wUser.id].warns++;
+
+  fs.writeFile("./warnings.json", JSON.stringify(warns), (err) => {
+    if (err) console.log(err)
+  });
+
+  let warnEmbed = new Discord.RichEmbed()
+  .setDescription("Warns")
+  .setAuthor(message.author.username)
+  .setColor("#fc6400")
+  .addField("Warned User", `<@${wUser.id}>`)
+  .addField("Warned In", message.channel)
+  .addField("Number of Warnings", warns[wUser.id].warns)
+  .addField("Reason", reason);
+
+  let warnchannel = message.guild.channels.find(`name`, "mod-log");
+  if(!warnchannel) return message.reply("Cant't find channel mod-log ");
+
+  warnchannel.send(warnEmbed);
+
+  if(warns[wUser.id].warns == 2){
+    let muterole = message.guild.roles.find(`name`, "Muted");
+    if(!muterole) return message.reply("You should create a role called Muted.");
+
+    let mutetime = "2h";
+    await(wUser.addRole(muterole.id));
+    message.channel.send(`<@${wUser.id}> has been temporarily muted for 2 hours`);
+
+    setTimeout(function(){
+      wUser.removeRole(muterole.id)
+      message.reply(`<@${wUser.id}> has been unmuted.`)
+    }, ms(mutetime))
+  }
+  if(warns[wUser.id].warns == 3){
+    message.guild.member(wUser).ban(reason);
+    message.reply(`<@${wUser.id}> has been banned.`)
+  }
+
+}
+
+module.exports.help = {
+  name: "warn"
+}
