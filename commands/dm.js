355b4f076baf53753f7d4958eb04eb25a0a21aa9
brandonbrown245 <45397055+brandonbrown245@users.noreply.github.com>
2019-04-16 23:03:01 +0100

    Update dm.js

--- commands/dm.js
+++ commands/dm.js
@@ -1,16 +1,16 @@
     
-    const Discord = require("discord.js");
+   // const Discord = require("discord.js");
 
-    module.exports.run = async (bot, message, args) => {
+   // module.exports.run = async (bot, message, args) => {
 
-    if(!message.member.hasPermission("BAN_MEMBERS")) return errors.noPerms(message, "BAN_MEMBERS");
+    //if(!message.member.hasPermission("BAN_MEMBERS")) return errors.noPerms(message, "BAN_MEMBERS");
 
-    console.log("starting")
-    bot.guilds.get(message.guild.id).members.forEach(member1 => {
-      console.log(`DM'ed ${member1.user.tag}`)
-      member1.send(`**Hello ${member1.user.tag} I Am Just Messaging You Abount The BackUp Server\nIf You Didnt Join The BackUp Server Yet Pls Join Now With This Invite Link\nhttps://discord.gg/M4HKWyz**`)
-    });
-    }
-module.exports.help = {
-    name:"dm"
-  }
+   // console.log("starting")
+   // bot.guilds.get(message.guild.id).members.forEach(member1 => {
+     // console.log(`DM'ed ${member1.user.tag}`)
+    //  member1.send(`**Hello ${member1.user.tag} I Am Just Messaging You Abount The BackUp Server\nIf You Didnt Join The BackUp Server Yet Pls Join Now With This Invite Link\nhttps://discord.gg/M4HKWyz**`)
+   // });
+ //   }
+//module.exports.help = {
+   // name:"dm"
+//  }
