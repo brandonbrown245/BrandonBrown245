01688d01ef86b79445107ef5c75ec4cd54159bd7
brandonbrown245 <45397055+brandonbrown245@users.noreply.github.com>
2019-04-16 13:07:42 +0100

    Update register.js

--- commands/register.js
+++ commands/register.js
@@ -1,26 +1,13 @@
 
-const Discord = require('discord.js');
+    const Discord = require('discord.js');
  
 exports.run = (client, message , args , tools ,) => {
  
        if (message.channel.name !== 'register') return message.reply('**You must Register your social club name in the register chat**');
 
 
-  const embed4 = new Discord.RichEmbed()
-  .setDescription("**Now that you are registered, please be sure to read the rules and how to join. Other than that, enjoy your time in the server and do not be afraid to ask any questions you may have**")
-  .setColor("#42f45c");
-  message.author.send({embed: embed4});
-
-
-  const embed3 = new Discord.RichEmbed()
-  .setDescription("**The money Dropper Sc name is Batman_345 he will not add you it is up to you to add the money Dropper\n\nHow to join the money drop lobby\n\nWhen you register you will get the sc of the money dropper you have to add him when you did add him. he will add you when he start's a money drop then you just join his lobby just if he says he is doing a money drop**")
-  .setColor("#42f45c");
-  message.author.send({embed: embed3});
- 
- 
- 
     let guild = message.guild;
-    const cnl = client.channels.get('536570203610415124');
+    const cnl = client.channels.get('561596580260478977');
     const embed = new Discord.RichEmbed()
     .setAuthor(`${message.author.tag}`)
     .addField('Social Club:', ` ${args}`)
@@ -36,6 +23,21 @@ exports.run = (client, message , args , tools ,) => {
     let removerole = message.guild.roles.find(role => role.name === 'Unregistered');
     message.member.removeRole(removerole);
 
+    const embed4 = new Discord.RichEmbed()
+    .setDescription("**Now that you are registered, please be sure to read the rules and how to join. Other than that, enjoy your time in the server and do not be afraid to ask any questions you may have**")
+    .setColor("#42f45c");
+    message.author.send({embed: embed4});
+  
+    const embed3 = new Discord.RichEmbed()
+    .setDescription("**The money Dropper Sc name is Batman_345 he will not add you it is up to you to add the money Dropper\n\nHow to join the money drop lobby\n\nWhen you register you will get the sc of the money dropper you have to add him when you did add him. he will add you when he start's a money drop then you just join his lobby just if he says he is doing a money drop**")
+    .setColor("#42f45c");
+    message.author.send({embed: embed3});
+
+    const embed1 = new Discord.RichEmbed()
+    .setDescription("**Money Drop Timetable\nMonday = Drop Starts At 4:00 Pm\nTuesday = No Money Drop\nWednesday = Drop Starts At 4:00 Pm\nThursday = No Money Drop\nFriday = Drop Starts At 4:00 Pm\nSaturday = No Money Drop\nSunday = Drop Starts At 4:00 Pm**")
+    .setColor("#42f45c");
+    message.author.send({embed: embed1});
+
     if (message.member.roles.has(role.id)) {
         let verifyEmbed = new Discord.RichEmbed()
             .setAuthor(message.member.displayName, message.author.displayAvatarURL)
@@ -48,9 +50,9 @@ exports.run = (client, message , args , tools ,) => {
             .setColor('#36393f')
             .setDescription('Your account has been successfully Registered.')
         return message.channel.send((verifyEmbed));
+      
     }
     
- 
   };
 
   exports.conf = {
